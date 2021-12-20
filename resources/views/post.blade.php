87ad42e964406f7d5165a7c8aaff2d669afce468
yhaae01 <suryaintpermana@gmail.com>
2021-12-20 19:10:21 +0700

    post category dan eloquent relationship

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,8 +2,8 @@
 
 @section('container')
 
-    <h2>{{ $post->title }}</h2>
-    
+    <h2 class="mb-5">{{ $post->title }}</h2>
+    <p>By: Surya in <a href="/categories/{{ $post->category['slug'] }}">{{ $post->category['name'] }}</a></p>
     {!! $post->body !!}
 
     <a href="/posts">Kembali</a>
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
-use App\Http\Controllers\PostController;
 use App\Models\Post;
+use App\Models\Category;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 Route::get('/', function () {
     return view('home',[
@@ -23,4 +24,19 @@ Route::get('/posts', [PostController::class, 'index']);
 
 // Single post
 Route::get('/posts/{post:slug}', [PostController::class, 'show']);
+
+Route::get('/categories', function(){
+    return view('categories', [
+        'title'      => 'Post Categories',
+        'categories' => Category::all()
+    ]);
+});
+
+Route::get('/categories/{category:slug}', function(Category $category){
+    return view('category', [
+        'title'     => $category->name,
+        'posts'     => $category->posts,
+        'category'  => $category->name
+    ]);
+});
 
