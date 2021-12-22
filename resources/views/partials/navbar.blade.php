c866d560746d55117db69d63f9cc54e898a7335c
yhaae01 <suryaintpermana@gmail.com>
2021-12-22 17:25:59 +0700

    Membuat dashboard admin

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -27,7 +27,7 @@
                         Welcome back, {{ auth()->user()->name }}
                     </a>
                     <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="/dashboard"><i class="bi bi-layout-text-sidebar-reverse"></i> My Dashboard</a></li>
+                        <li><a class="dropdown-item" href="/dashboard"><i class="bi bi-house-door"></i> My Dashboard</a></li>
                         <li><hr class="dropdown-divider"></li>
                         <li>
                             <form action="/logout" method="POST">
